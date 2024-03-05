36c8ad9dec2d3bcc54d387ff558d38cafedd6aca
oussamaelhousni <oussamaelhousnioe@gmail.com>
2024-03-05 01:47:21 +0100

    create scrollable pages

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  FlatList,
+  Image,
+  View,
+  SafeAreaView,
+  useWindowDimensions,
+} from "react-native";
+import data from "./data/data.js";
 
 export default function App() {
+  const { width: SCREEN_WIDTH } = useWindowDimensions();
+  const RenderItem = ({ item, index }) => {
+    return (
+      <View style={[styles.itemContainer, { width: SCREEN_WIDTH }]}>
+        <Image
+          source={item.image}
+          style={[{ width: SCREEN_WIDTH * 0.8, height: SCREEN_WIDTH * 0.8 }]}
+        />
+        <View>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+          <Text style={styles.itemText}>{item.text}</Text>
+        </View>
+      </View>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={data}
+        renderItem={({ item, index }) => (
+          <RenderItem index={index} item={item} />
+        )}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f8e9b0",
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  itemTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 25,
+  },
+  itemText: {
+    fontSize: 16,
+    textAlign: "center",
   },
 });
